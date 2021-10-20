e854a0cd20d2273ec94a7ab72adbbcd70ab09be4
JonesGladston <jones.gladiator@gmail.com>
2021-10-20 13:14:46 +0530

    Initialized a nuxt3-app to declare global variables

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="index-container">
+    <h1> This is the Index page</h1>
+  </div>
+</template>
+
+<style lang="scss">
+  .index-container {
+    color: $indexPageColor;
+  }
+</style>
+
---
+++ pages/new.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="new-container">
+    <h1> A new page to check out the global variable declaration</h1>
+  </div>
+</template>
+
+<style lang="scss">
+  .new-container {
+    color: $newPageColor;
+  }
+</style>
+
